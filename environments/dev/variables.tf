48c34f94071c7219756145224cb3e9ded6b62c53
santhosh <santhosh93499@gmail.com>
2025-08-27 23:03:56 +0100

    Update VPC variable mappings to include environment-specific CIDR blocks

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -14,8 +14,8 @@ variable "vpcs" {
   description = "Map of VPC names to their CIDR blocks"
   type        = map(string)
   default = {
-    "vpc-10"  = "10.0.0.0/16"
-    "vpc-172" = "172.0.0.0/16"
-    "vpc-192" = "192.0.0.0/16"
+    "prod"  = "10.0.0.0/16"
+    "dev" = "172.0.0.0/16"
+    "inspection" = "192.0.0.0/16"
   }
 }
